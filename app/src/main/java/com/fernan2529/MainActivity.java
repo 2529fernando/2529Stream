ed3245642f67e557f5b40142fa473850758d520a
Fernando Montalvo <fernansebas96@gmail.com>
2025-09-17 04:27:17 -0500

    commit 2529musicafinal2

--- app/src/main/java/com/fernan2529/MainActivity.java
+++ app/src/main/java/com/fernan2529/MainActivity.java
@@ -186,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupHeaderAndGridButtons() {
         SparseArray<Class<?>> map = new SparseArray<>();
         // Cabecera
-        map.put(R.id.donaciones,       Reproductor.class);
+        map.put(R.id.donaciones,       donaciones.class);
         map.put(R.id.btn_juegos,       JuegosActivity.class);
         map.put(R.id.potifyy,          MusicActivity.class);
         map.put(R.id.btnchat,          SplashActivity.class);
